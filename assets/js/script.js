d7241834c109a9699f985208d54b3237537407e4
ZQWhiting <zach.whiting@icloud.com>
2020-06-01 12:41:58 -0600

    getWeather from OpenWeather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,24 @@ $('#city-submit').on('click', function (event) {
         alert(`Please enter a city`)
     }
 })
+
+const getWeather = function (city) {
+    // format the github api url
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data, city);
+                });
+            } else {
+                alert(`Error: ${response.statusText}`);
+            }
+        })
+        .catch(function (error) {
+            alert(`Unable to connect to OpenWeather`);
+        });
+};
