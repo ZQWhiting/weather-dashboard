2a509275a346da1421bf0399a31484b1835b199c
ZQWhiting <zach.whiting@icloud.com>
2020-06-01 15:23:38 -0600

    current weather now diplays uv index value with color indication

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ $('#city-submit').on('click', function (event) {
 })
 
 const getWeather = function (city) {
-    // format the github api url
+    // format the openweather api url
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     // make a request to the url
@@ -23,9 +23,8 @@ const getWeather = function (city) {
         .then(function (response) {
             // request was successful
             if (response.ok) {
-                response.json().then(function (data) {
-                    /* getUv(data); */
-                    displayWeather(data);
+                response.json().then(function (currentWeather) {
+                    getUv(currentWeather);
                 });
             } else {
                 alert(`Error: ${response.statusText}`);
@@ -36,21 +35,57 @@ const getWeather = function (city) {
         });
 };
 
-const displayWeather = function (weather) {
+const getUv = function (currentWeather) {
+    const lat = currentWeather.coord.lat;
+    const lon = currentWeather.coord.lon;
+    const apiUrl = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`
+
+    fetch(apiUrl)
+        .then(function (response) {
+            // request was successful
+            if (response.ok) {
+                response.json().then(function (uvIndex) {
+                    displayWeather(currentWeather, uvIndex)
+                });
+            } else {
+                alert(`Error: ${response.statusText}`);
+            }
+        })
+        .catch(function (error) {
+            alert(`Unable to connect to OpenWeather`);
+        });
+};
+
+const displayWeather = function (currentWeather, uvIndex) {
     const date = moment().format('D/M/YYYY');
-    const city = weather.name;
+    const city = currentWeather.name;
     $('#solo-header').text(`${city} (${date})`);
 
-    const iconCode = weather.weather[0].icon;
+    const iconCode = currentWeather.weather[0].icon;
     const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`
     const iconEl = $('<img>').attr('src', iconUrl);
     iconEl.appendTo($('#solo-header'));
 
-    const temp = weather.main.temp.toFixed(1);
-    const wind = weather.wind.speed.toFixed(1);
-    const humid = weather.main.humidity;
+    const temp = currentWeather.main.temp.toFixed(1);
+    const wind = currentWeather.wind.speed.toFixed(1);
+    const humid = currentWeather.main.humidity;
     $('#solo-temp').text(`Temperature: ${temp} ºF`)
     $('#solo-humid').text(`Humidity: ${humid}%`)
     $('#solo-wind').text(`Wind Speed: ${wind} MPH`)
     $('#solo-uv').text(`UV Index: `)
-}
+
+    const uvIndexEl = $('<span>')
+        .text(uvIndex.value)
+        .addClass('rounded text-white p-1');
+
+    if (uvIndexEl.text() <= 2) {
+        uvIndexEl.addClass('bg-success');
+    }
+    else if (uvIndexEl.text() <= 5) {
+        uvIndexEl.addClass('bg-warning');
+    }
+    else {
+        uvIndexEl.addClass('bg-danger');
+    }
+    uvIndexEl.appendTo($('#solo-uv'));
+};
